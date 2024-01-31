797c0c5e0dc8b76b40b438eee16743064de50740
Phiwe Mazwi <Phiwe.Mazwi@eoh.com>
2024-01-31 02:11:39 +0200

    Create initial project structure

---
+++ src/main/java/tech/ioco/robot/apocalypse/ApocalypseApplication.java
@@ -0,0 +1,13 @@
+package tech.ioco.robot.apocalypse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApocalypseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApocalypseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/ioco/robot/apocalypse/ApocalypseApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ioco.robot.apocalypse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApocalypseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
